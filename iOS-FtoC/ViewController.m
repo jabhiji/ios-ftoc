828dcf1a7b6226610a3ef620ea4412b9e7e2c0c5
Abhijit Joshi <joshi1974@gmail.com>
2014-04-25 10:13:49 -0400

    initial working version

--- iOS-FtoC/ViewController.m
+++ iOS-FtoC/ViewController.m
@@ -9,10 +9,17 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *valF;
+@property (weak, nonatomic) IBOutlet UITextField *valC;
+- (IBAction)convertToC:(id)sender;
+- (IBAction)convertToF:(id)sender;
 
 @end
 
 @implementation ViewController
+
+@synthesize valF;
+@synthesize valC;
 
 - (void)viewDidLoad
 {
@@ -26,4 +33,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)convertToC:(id)sender {
+    float TF = [valF.text floatValue];
+    float TC = (TF - 32.0)*5.0/9.0;
+    valC.text = [NSString stringWithFormat:@"%f",TC];
+}
+
+- (IBAction)convertToF:(id)sender {
+    float TC = [valC.text floatValue];
+    float TF = 9.0*TC/5.0 + 32.0;
+    valF.text = [NSString stringWithFormat:@"%f",TF];
+}
 @end
