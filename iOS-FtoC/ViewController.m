a490e8aad51970914e7f0b73052499141dbd8129
Abhijit Joshi <joshi1974@gmail.com>
2014-04-25 10:48:54 -0400

    further improvements to UI - also added message to user

--- iOS-FtoC/ViewController.m
+++ iOS-FtoC/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *valF;
 @property (weak, nonatomic) IBOutlet UITextField *valC;
+@property (weak, nonatomic) IBOutlet UILabel *message;
 - (IBAction)convertToC:(id)sender;
 - (IBAction)convertToF:(id)sender;
 
@@ -20,6 +21,7 @@
 
 @synthesize valF;
 @synthesize valC;
+@synthesize message;
 
 - (void)viewDidLoad
 {
@@ -37,11 +39,25 @@
     float TF = [valF.text floatValue];
     float TC = (TF - 32.0)*5.0/9.0;
     valC.text = [NSString stringWithFormat:@"%.1f",TC];
+    if (TF > 80) {
+        message.text = [NSString stringWithFormat:@"Getting hot out there.."];
+    } else if (TF > 70 && TF < 80) {
+        message.text = [NSString stringWithFormat:@"Nice weather for biking.."];
+    } else {
+        message.text = [NSString stringWithFormat:@"Seems a little chilly.."];
+    }
 }
 
 - (IBAction)convertToF:(id)sender {
     float TC = [valC.text floatValue];
     float TF = 9.0*TC/5.0 + 32.0;
     valF.text = [NSString stringWithFormat:@"%.1f",TF];
+    if (TF > 80) {
+        message.text = [NSString stringWithFormat:@"Getting hot out there.."];
+    } else if (TF > 70 && TF < 80) {
+        message.text = [NSString stringWithFormat:@"Nice weather for biking.."];
+    } else {
+        message.text = [NSString stringWithFormat:@"Seems a little chilly.."];
+    }
 }
 @end
